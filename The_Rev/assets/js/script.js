95e6808c8a193910f94758715130eb6bb111145e
Felipepereiralima <felipeplima@live.com>
2021-11-30 16:16:34 -0300

    Adicionando zoom no mapa

--- The_Rev/assets/js/script.js
+++ The_Rev/assets/js/script.js
@@ -49,6 +49,16 @@ function enviar(){
     }
 }
 
+function mapaZoom(){
+    mapa.style.width = "800px"
+    mapa.style.height= "600px"
+}
+
+function mapaNormal(){
+    mapa.style.width = "400px"
+    mapa.style.height= "250px"
+}
+
 let nomes = window.document.getElementById("nome")
 let email = document.querySelector("#email")
 let assunto = document.querySelector("#assunto")
